541936c0ef4e725bafbd727b1ff02a2dcd0f808d
Yehonathan Sharvit <viebel@gmail.com>
2014-11-29 21:51:13 +0200

    fix function description

--- src/cljs/klozzer/protocols.cljs
+++ src/cljs/klozzer/protocols.cljs
@@ -3,7 +3,7 @@
 
 (defprotocol IFileSystem
   (-write [this filename data] "Writes a file with data")
-  (-file-entry [this filename create?] "Retrieves the fileEntry javascript object")
-  (-file [this filename] "Retrieves the file javascript object")
+  (-file-entry [this filename create?] "Retrieves the fileEntry object")
+  (-file [this filename] "Retrieves the file data")
   (-url [this filename] "Retrieves the url of the file")
-  (-read [this filename format] "Reads a file"))
+  (-read [this filename format] "Reads a file. format can be text or arraybuffer"))
