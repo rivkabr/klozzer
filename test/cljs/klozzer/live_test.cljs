67b0dce27a11bed5f23cc7b77c0f6189d97424fb
Yehonathan Sharvit <viebel@gmail.com>
2014-11-30 21:54:57 +0200

    fix live_test.cljs

--- test/cljs/klozzer/live_test.cljs
+++ test/cljs/klozzer/live_test.cljs
@@ -1,14 +1,16 @@
-(ns test.live2
+(ns test.live3
   (:use-macros [cljs.core.async.macros :only [go go-loop]])
   (:require [klozzer.net :as net]))
+
+(js/alert 3)
+
 
 (enable-console-print!)
 
 
 (net/init-fs 10000)
-(js/alert 3)
 (go
-   (println (<! (net/get-cached-url ["http://localhost:3000" :google "text"]))))
+   (println (<! (net/get-cached-url "http://localhost:4567" "google" "text"))))
 
 
 
